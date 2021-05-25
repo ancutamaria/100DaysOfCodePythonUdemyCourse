834c0c7c2d9ada9aa120f994d51965cd1b456bf5
ancuta <ancuta.maria@gmail.com>
2021-05-25 12:39:30 +0300

    Beginner - Higher Lower Game Project

---
+++ day_14_higher-lower-game/main.py
@@ -0,0 +1,32 @@
+import art
+import game_data
+import random
+
+current_score = 0
+print(art.logo)
+while True:
+    celebrityA = random.choice(game_data.data)
+    celebrityB = random.choice(game_data.data)
+    while celebrityA.get("name") == celebrityB.get("name"):
+        celebrityB = random.choice(game_data.data)
+
+    print(f"Compare A: ")
+    print(f"{celebrityA.get('name')}, a {celebrityA.get('description')}, from {celebrityA.get('country')}")
+    print(art.vs)
+    print(f"Compare B: ")
+    print(f"{celebrityB.get('name')}, a {celebrityB.get('description')}, from {celebrityB.get('country')}")
+    print()
+    response = input("Who has more followers? Type 'A' or 'B': ")
+    hasAmoreFollowersThanB = celebrityA.get("follower_count") > celebrityB.get("follower_count")
+    print(art.logo)
+    print()
+    if hasAmoreFollowersThanB and response == "A":
+        current_score += 1
+        print(f"You're write! Current score: {current_score}.")
+    elif not hasAmoreFollowersThanB and response == "B":
+        current_score += 1
+        print(f"You're write! Current score: {current_score}.")
+    else:
+        current_score = 0
+        print(f"Sorry, that's wrong. Current score: 0")
+    print()
